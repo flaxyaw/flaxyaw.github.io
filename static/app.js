6c068451ce2885980b3d15efd682fde3e0acef06
JustMiraii <75630937+IamUndetected@users.noreply.github.com>
2022-01-06 03:41:40 +0100

    fsdf

--- static/app.js
+++
@@ -1,8 +0,0 @@
-const bg = document.getElementById('bgm');
-bg.volume = 0.1;
-
-//listens for click
-document.addEventListener('click', function () {
-	bg.play();
-	document.getElementById('click').hidden = true;
-});
